4af0e5c2eff83cac4f27af923d94d4f64aa511ca
NikaS <n.simonishvili@sweeftdigital.com>
2022-10-27 17:00:22 +0400

    profile page

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -6,7 +6,8 @@
     </x-slot>
 
     <div class="container">
-asdasd
+        <img src="{{ asset('storage/avatar-'. auth()->id(). '.png') }}" width="100" class="mr-2"/>
+        <h1 class="mt-3">{{ auth()->user()->name }}</h1>
     </div>
 </x-app-layout>
 
